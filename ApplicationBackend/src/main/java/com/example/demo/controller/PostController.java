681c8ad482828ef01c7aefc338c733faf12eccf4
Vyanketesh Nagpure <vyanketeshnagpure123@gmail.com>
2023-11-24 17:21:42 +0530

    added comment count update

--- ApplicationBackend/src/main/java/com/example/demo/controller/PostController.java
+++ ApplicationBackend/src/main/java/com/example/demo/controller/PostController.java
@@ -1,19 +1,18 @@
 package com.example.demo.controller;
 
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.example.demo.dao.Comment;
 import com.example.demo.model.Post;
@@ -67,7 +66,7 @@ public class PostController {
 		List<Comment> allComments = requiredPost.getComments();
 		
 		allComments.add(commentBody);
-		
+		requiredPost.setNoOfComments(requiredPost.getNoOfComments()+1);
 		
 		requiredPost.setComments(allComments);
 		
